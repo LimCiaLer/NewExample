9ea3b5d7a875251e5d2eb27f49f9c4bd20ccea6a
Tarc <Tarc@B108>
2018-10-29 11:19:39 +0800

    my first example

--- src/mynewagile/MyNewAgile.java
+++ src/mynewagile/MyNewAgile.java
@@ -15,7 +15,8 @@ public class MyNewAgile {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello world1");
+        System.out.println("Hello world2");
     }
     
 }
